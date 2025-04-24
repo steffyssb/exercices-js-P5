3ac217dd97df5fe59bd68f5ba09c256cdd85d59f
steffyssb <steffyssb6492@gmail.com>
2025-04-24 20:18:31 +0200

    new
    updates

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,10 @@
-//votre code ici
-
-export default printNumbers
+// Votre code ici
+function printNumbers(n) {
+    let result = "";
+    for (let i = 1; i <= n; i++) {
+      result += i + " ";
+    }
+    return result.trim(); // To remove the trailing space
+  }
+  
+export default printNumbers;
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,12 +1,12 @@
-// votre code ici
-function checkAge(age) {
+ function checkAge(age) {
     if (age < 18) {
-        console.log("Vous êtes mineur.");
-    } else if (age >= 18 && age <= 65) {
-        console.log("Vous êtes majeur.");
+      return "Vous êtes mineur.";
+    } else if (age < 65 ) {
+      return "Vous êtes majeur.";
     } else {
-        console.log("Vous êtes senior.");
+      return "Vous êtes senior.";
     }
-}
-
-export default checkAge;
+  }
+  export default checkAge ;
+  
+  
